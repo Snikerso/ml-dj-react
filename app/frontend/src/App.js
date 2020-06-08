f6e17839719efdfbb25f4f6a39054489ea4036cf
Snikerso <42245963+Snikerso@users.noreply.github.com>
2020-06-08 13:13:16 +0200

    day1

---
+++ app/frontend/src/App.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+import DropTarget from './components/DropTarget';
+import DropTraining from './components/DropTraining';
+
+
+function App() {
+
+
+
+  return (
+    
+    <div className="App">
+      <DropTarget />
+      <DropTraining />
+    </div>
+
+
+  );
+}
+
+export default App;
---
+++ app/frontend/src/components/DropTraining.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import { useDropzone } from 'react-dropzone';
+function DropTraining(props) {
+
+    const [files, setFiles] = useState([]);
+
+    const { getRootProps, getInputProps } = useDropzone({
+        accept: 'image/*',
+        onDrop: acceptedFiles => {
+            setFiles(acceptedFiles.map(file => Object.assign(file, {
+                preview: URL.createObjectURL(file)
+            })));
+        }
+    });
+
+    const thumbs = files.map(file => (
+        <div key={file.name}>
+            <div >
+                <img
+                    src={file.preview}
+
+                />
+            </div>
+        </div>
+    ));
+
+    useEffect(() => () => {
+        // Make sure to revoke the data uris to avoid memory leaks
+        files.forEach(file => URL.revokeObjectURL(file.preview));
+    }, [files]);
+
+    return (
+        <section className="container">
+            <div {...getRootProps({ className: 'dropzone' })}>
+                <input {...getInputProps()} />
+                <p>Drag 'n' drop some files here, or click to select files</p>
+            </div>
+            <aside>
+                <h4>Files</h4>
+                <ul>{thumbs}</ul>
+            </aside>
+        </section>
+    );
+}
+
+export default DropTraining
